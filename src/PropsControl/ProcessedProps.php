055db6027f1cdab1754bd5f80036a923a76014d3
Ondrej komarek <okomarek@simpliko.cz>
2024-08-21 12:59:10 +0200

    remove SmartObject from processed props

--- src/PropsControl/ProcessedProps.php
+++ src/PropsControl/ProcessedProps.php
@@ -1,41 +1,80 @@
-<?php declare (strict_types = 1);
+<?php declare (strict_types=1);
 
 namespace Wavevision\PropsControl;
 
-use Nette\SmartObject;
+use Wavevision\PropsControl\Exceptions\NotAllowed;
 
 /**
  * @internal
  */
 final class ProcessedProps
 {
+    /**
+     * @var mixed[]
+     */
+    private array $values;
 
-	use SmartObject;
+    public function __construct()
+    {
+        $this->values = [];
+    }
 
-	/**
-	 * @var mixed[]
-	 */
-	private array $values;
+    /**
+     * @return mixed[]
+     */
+    public function getValues(): array
+    {
+        return $this->values;
+    }
 
-	public function __construct()
-	{
-		$this->values = [];
-	}
+    /**
+     * @param mixed $value
+     */
+    public function __set(string $name, $value): void
+    {
+        $this->values[$name] = $value;
+    }
 
-	/**
-	 * @return mixed[]
-	 */
-	public function getValues(): array
-	{
-		return $this->values;
-	}
+    /**
+     * @return mixed
+     */
+    public function &__get(string $name)
+    {
+        if (!$this->isSet($name)) {
+            throw new NotAllowed("Cannot read an undeclared prop '$name'.");
+        }
+        return $this->values[$name];
+    }
 
-	/**
-	 * @param mixed $value
-	 */
-	public function __set(string $name, $value): void
-	{
-		$this->values[$name] = $value;
-	}
+    public function __isset(string $name): bool
+    {
+        return $this->isSet($name);
+    }
+
+    public function __unset(string $name): void
+    {
+        throw new NotAllowed("Cannot unset prop '$name', props are read-only.");
+    }
+
+    public function isSet(string $prop): bool
+    {
+        return array_key_exists($prop, $this->values);
+    }
+
+    /**
+     * @param mixed[] $arguments
+     */
+    public function __call(string $method, array $arguments): void
+    {
+        throw new NotAllowed("Cannot call an undefined method '$method'.");
+    }
+
+    /**
+     * @param mixed[] $arguments
+     */
+    public static function __callStatic(string $method, array $arguments): void
+    {
+        throw new NotAllowed("Cannot call an undefined static method '$method'.");
+    }
 
 }
